800aec0af519cb9eb98352ad10ff6ed41ca84652
王泳淇 <815099246@qq.com>
2023-12-10 13:03:30 +0800

    commit

---
+++ app/src/main/java/io/ecosed/common_example/PluginExample.kt
@@ -0,0 +1,20 @@
+package io.ecosed.common_example
+
+import android.app.Activity
+import androidx.lifecycle.Lifecycle
+import io.ecosed.common.FlutterPluginProxy
+
+class PluginExample : FlutterPluginProxy {
+
+    override fun getActivity(activity: Activity) {
+
+    }
+
+    override fun getLifecycle(lifecycle: Lifecycle) {
+
+    }
+
+    override fun onMethodCall() {
+
+    }
+}
---
+++ common/src/main/kotlin/io/ecosed/common/FlutterPluginProxy.kt
@@ -0,0 +1,10 @@
+package io.ecosed.common
+
+import android.app.Activity
+import androidx.lifecycle.Lifecycle
+
+interface FlutterPluginProxy {
+    fun getActivity(activity: Activity)
+    fun getLifecycle(lifecycle: Lifecycle)
+    fun onMethodCall()
+}

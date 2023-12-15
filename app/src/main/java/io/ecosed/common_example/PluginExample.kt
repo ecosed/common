d51f6f9808ac39ea6bb1935f23538a019b4a1fbf
王泳淇 <815099246@qq.com>
2023-12-15 14:31:54 +0800

    commit

--- app/src/main/java/io/ecosed/common_example/PluginExample.kt
+++ app/src/main/java/io/ecosed/common_example/PluginExample.kt
@@ -9,20 +9,22 @@ import io.ecosed.common.ResultProxy
 class PluginExample : FlutterPluginProxy {
 
     override fun getActivity(activity: Activity) {
-
+        // 获取activity
     }
 
     override fun getLifecycle(lifecycle: Lifecycle) {
-
+        // 获取生命周期
     }
 
     override fun attach() {
-
+        // 初始化
     }
 
     override fun onMethodCall(call: MethodCallProxy, result: ResultProxy) {
         when (call.method) {
-            "" -> result.success("")
+            "success" -> result.success("success")
+            "bundle" -> result.success(call.bundle?.getString("bundle"))
+            "error" -> result.error("error", "error", "error")
             else -> result.notImplemented()
         }
     }
